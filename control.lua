dfddef80b40b3f1f6e09793e90dd08aab0d0e1ab
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-10-30 11:01:29 -0500

    TRAILING SPACE BEGONE!

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 script.on_configuration_changed(function (data)
-    if data.mod_changes["ConfigurableVehicles"] ~= nil or data.mod_changes["ConfigurableVehicles"].old_version ~= nil then 
+    if data.mod_changes["ConfigurableVehicles"] ~= nil or data.mod_changes["ConfigurableVehicles"].old_version ~= nil then
         if data.mod_changes["ConfigurableVehicles"].old_version == "1.1.1" and settings.startup["tank-remote-driving"].value == false then
             for k, p in pairs(game.players) do
                 game.players[k].print("[color=55,117,47]Message from Configurable Vehicles:[/color] Tank remote driving is currently set to [color=1,0,0]False[/color], which was previously default for this mod. The vanilla setting which is now default is True and is recommended for Space Age. This message will not display after your next save.",
